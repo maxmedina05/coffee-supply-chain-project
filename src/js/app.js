3e76dba4f3d757712ff890e20e995a16711c94ff
Max Medina <maxmedina05@gmail.com>
2020-05-07 18:26:28 +0200

    deploy to rinkeby

--- src/js/app.js
+++ src/js/app.js
@@ -95,27 +95,26 @@ App = {
   },
 
   initWeb3: async function () {
-    /// Find or Inject Web3 Provider
-    /// Modern dapp browsers...
-    // if (window.ethereum) {
-    //     App.web3Provider = window.ethereum;
-    //     try {
-    //         // Request account access
-    //         await window.ethereum.enable();
-    //     } catch (error) {
-    //         // User denied account access...
-    //         console.error("User denied account access")
-    //     }
-    // }
-    // // Legacy dapp browsers...
-    // else if (window.web3) {
-    //     App.web3Provider = window.web3.currentProvider;
-    // }
-    // // If no injected web3 instance is detected, fall back to Ganache
-    // else {
-    //     App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-    // }
-    App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545')
+    // Find or Inject Web3 Provider
+    // Modern dapp browsers...
+    if (window.ethereum) {
+        App.web3Provider = window.ethereum;
+        try {
+            // Request account access
+            await window.ethereum.enable();
+        } catch (error) {
+            // User denied account access...
+            console.error("User denied account access")
+        }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+        App.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+        App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545')
+    }
 
     App.getMetaskAccountID()
 
